bcdaf3cc076780a2611ffd6f68342e8c40ba6c44
Mikolaj <pastuchamikolaj0@gmail.com>
2024-04-15 13:21:21 +0200

    Integration

---
+++ blebox_uniapi/jfollow.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from typing import Any
+
+import jmespath
+
+
+def follow(data: dict | list, path: str) -> Any:
+
+    if data is None:
+        raise RuntimeError(f"bad argument: data {data}")  # pragma: no cover
+    
+    expression = jmespath.compile(path)
+    return expression.search(data)
